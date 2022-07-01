e43071b57d9b6cf570ff2578de9f16be20302fe4
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-07-01 15:55:21 +0200

    feat: forms

--- src/forms.js
+++ src/forms.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { Form, Container } from "react-bootstrap";
+import { Form, Container, InputGroup, FormControl } from "react-bootstrap";
 
 export default function RbForms() {
   return (
     <div>
-      <Form>
-        <Form.Group>
-          <Form.Label controlId="formEmail" style={{ color: "#fff" }}>
-            Email address
-          </Form.Label>
+      <Form style={{ color: "white" }}>
+      <h1 className="mt-5 mb-3 white">Basic form</h1>
+
+        <Form.Group controlId="formEmail">
+          <Form.Label style={{ color: "#fff" }}>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter an email" size="lg" />
           <Form.Text className="text-muted">
             We'll never share your message
           </Form.Text>
         </Form.Group>
 
-        <Form.Group>
-          <Form.Label controlId="formBasicPassword" style={{ color: "#fff" }}>
-            Password
-          </Form.Label>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label style={{ color: "#fff" }}>Password</Form.Label>
           <Form.Control type="password" placeholder="Password"></Form.Control>
         </Form.Group>
 
@@ -32,12 +30,78 @@ export default function RbForms() {
           <Form.Control type="file" size="lg" />
         </Form.Group>
 
-    <Form.Group>
-        <Form.Label htmlFor="exampleColorInput">Color picker</Form.Label>
-        <Form.Control type="color" id="exampleColorInput" defaultValue="#563d7c"
-        title="Choose your color"
-        />
-    </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="exampleColorInput">Color picker</Form.Label>
+          <Form.Control
+            type="color"
+            id="exampleColorInput"
+            defaultValue="#563d7c"
+            title="Choose your color"
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Select aria-label="Default select example">
+            <Form.Label style={{ color: "#fff" }}>Select the role</Form.Label>
+            <option>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+          </Form.Select>
+        </Form.Group>
+
+        {/* inline radios and checkboxes */}
+        <h1 className="mt-5 mb-3 white"> Inline Checkboxes and radios</h1>
+        {["checkbox", "radio"].map((type) => (
+          <div key={`inline-${type}`} className="mb-3">
+            <Form.Check
+              inline
+              label="1"
+              name="grp1"
+              type={type}
+              id={`inline-${type}-1`}
+            />
+            <Form.Check
+              inline
+              label="2"
+              name="grp1"
+              type={type}
+              id={`inline-${type}-1`}
+            />
+            <Form.Check
+              inline
+              label="3"
+              type={type}
+              id={`inline-${type}-1`}
+              disabled
+            />
+          </div>
+        ))}
+
+        <h1 className="mt-5 mb-3 white">InputGroup section</h1>
+        {/* input group */}
+        <InputGroup>
+          <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+          <FormControl
+            placeholder="username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+
+        <InputGroup className="my-4">
+          <FormControl
+            placeholder="Recipient's username"
+            aria-label="Recipient's username"
+            aria-describedby="basic-addon2"
+          />
+          <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+        </InputGroup>
+
+        <InputGroup className="my-4">
+          <InputGroup.Text>$</InputGroup.Text>
+          <FormControl placeholder="amount" />
+          <InputGroup.Text>.00</InputGroup.Text>
+        </InputGroup>
       </Form>
     </div>
   );
